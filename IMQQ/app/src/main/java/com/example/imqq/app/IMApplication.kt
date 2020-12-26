f9273bcd2efbd9bac4e2c452bfde546d3cbebbd7
z1572903465 <1572903465@qq.com>
2020-12-26 15:59:40 +0800

    Bomb SDk的集成

--- IMQQ/app/src/main/java/com/example/imqq/app/IMApplication.kt
+++ IMQQ/app/src/main/java/com/example/imqq/app/IMApplication.kt
@@ -1,6 +1,7 @@
 package com.example.imqq.app
 
 import android.app.Application
+import cn.bmob.v3.Bmob
 import com.example.imqq.BuildConfig
 import com.hyphenate.chat.EMClient
 import com.hyphenate.chat.EMOptions
@@ -13,7 +14,7 @@ class IMApplication: Application() {
         EMClient.getInstance().init(applicationContext, EMOptions());
         //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
         EMClient.getInstance().setDebugMode(BuildConfig.DEBUG);
-//        Bmob.initialize(applicationContext, "df5a02bd4c7875549784ceb033407cc4")
+        Bmob.initialize(applicationContext, "1896051197272c3c95fa08775428a9d9")
 
 //        EMClient.getInstance().chatManager().addMessageListener(messageListener)
     }
