7cd1949039a69cc119e63fcef6904f2be2f6ef63
z1572903465 <1572903465@qq.com>
2020-12-26 09:44:09 +0800

    Splash界面MVP协议

--- IMQQ/app/src/main/java/com/example/imqq/contract/BasePresenter.kt
+++ IMQQ/app/src/main/java/com/example/imqq/contract/BasePresenter.kt
@@ -1,12 +1,5 @@
 package com.example.imqq.contract
 
 interface BasePresenter {
-    interface Presenter: BasePresenter {
-        fun checkLoginStatus() //检查登录状态
-    }
 
-    interface View{
-        fun onNotLoggedIn() //没有登陆的ui处理
-        fun onLoggedIn()  //已经登录的ui处理
-    }
 }
--- IMQQ/app/src/main/java/com/example/imqq/contract/SplashContract.kt
+++ IMQQ/app/src/main/java/com/example/imqq/contract/SplashContract.kt
@@ -1,4 +1,12 @@
 package com.example.imqq.contract
 
 interface SplashContract {
+    interface Presenter: BasePresenter {
+        fun checkLoginStatus() //检查登录状态
+    }
+
+    interface View{
+        fun onNotLoggedIn() //没有登陆的ui处理
+        fun onLoggedIn()  //已经登录的ui处理
+    }
 }
