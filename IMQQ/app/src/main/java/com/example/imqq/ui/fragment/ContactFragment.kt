c36582ad56c4d2b3f15f22a8f1f383742e4c79ce
z1572903465 <1572903465@qq.com>
2020-12-26 21:47:56 +0800

    联系人界面初始化

--- IMQQ/app/src/main/java/com/example/imqq/ui/fragment/ContactFragment.kt
+++ IMQQ/app/src/main/java/com/example/imqq/ui/fragment/ContactFragment.kt
@@ -1,7 +1,20 @@
 package com.example.imqq.ui.fragment
 
+import android.view.View
 import com.example.imqq.R
+import kotlinx.android.synthetic.main.fragment_contacts.*
+import kotlinx.android.synthetic.main.header.*
 
 class ContactFragment:BaseFragment() {
     override fun getLayoutResId(): Int = R.layout.fragment_contacts
+    override fun init() {
+        super.init()
+        headerTitle.text = getString(R.string.contact)
+        add.visibility = View.VISIBLE
+        swipeRefreshLayout.apply {
+            setColorSchemeResources(R.color.qq_blue)
+            swipeRefreshLayout.isRefreshing = true
+        }
+
+    }
 }
