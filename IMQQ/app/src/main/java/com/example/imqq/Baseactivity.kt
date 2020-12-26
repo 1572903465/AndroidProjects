e0dc1692d5262ab9f7ae7017f1bd3d5953186738
z1572903465 <1572903465@qq.com>
2020-12-26 08:45:12 +0800

    初始化项目

---
+++ IMQQ/app/src/main/java/com/example/imqq/Baseactivity.kt
@@ -0,0 +1,24 @@
+package com.example.imqq
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class Baseactivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayoutResId())
+        init()
+    }
+
+    private fun init() {
+        TODO("Not yet implemented")
+        // 初始化一些公共的功能
+    }
+
+    //子类返回一个布局
+    abstract fun getLayoutResId(): Int
+
+
+
+}
