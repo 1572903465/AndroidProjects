5f05b28463fbe95620245017c00226314e6ccbe1
z1572903465 <1572903465@qq.com>
2020-12-26 21:35:26 +0800

    动态界面布局

--- IMQQ/app/src/main/java/com/example/imqq/ui/fragment/DynamicFragment.kt
+++ IMQQ/app/src/main/java/com/example/imqq/ui/fragment/DynamicFragment.kt
@@ -1,7 +1,16 @@
 package com.example.imqq.ui.fragment
 
 import com.example.imqq.R
+import com.hyphenate.chat.EMClient
+import kotlinx.android.synthetic.main.fragment_dynamic.*
+import kotlinx.android.synthetic.main.header.*
 
 class DynamicFragment:BaseFragment() {
     override fun getLayoutResId(): Int = R.layout.fragment_dynamic
+    override fun init() {
+        super.init()
+        headerTitle.text = getString(R.string.dynamic)
+        val logouString = String.format(getString(R.string.logout),EMClient.getInstance().currentUser)
+        logout.text = logouString
+    }
 }
