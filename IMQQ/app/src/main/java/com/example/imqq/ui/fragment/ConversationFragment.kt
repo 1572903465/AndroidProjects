046bb230e52cce57a9814d2b90b80e0ebfe5d514
z1572903465 <1572903465@qq.com>
2020-12-27 20:11:18 +0800

    收到消息会话界面列表的刷新

--- IMQQ/app/src/main/java/com/example/imqq/ui/fragment/ConversationFragment.kt
+++ IMQQ/app/src/main/java/com/example/imqq/ui/fragment/ConversationFragment.kt
@@ -3,8 +3,10 @@ package com.example.imqq.ui.fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.imqq.R
 import com.example.imqq.adapter.ConversationListAdapter
+import com.example.imqq.adapter.EMMessageListenerAdapter
 import com.hyphenate.chat.EMClient
 import com.hyphenate.chat.EMConversation
+import com.hyphenate.chat.EMMessage
 import kotlinx.android.synthetic.main.fragment_conversation.*
 import kotlinx.android.synthetic.main.header.*
 import org.jetbrains.anko.doAsync
@@ -14,6 +16,13 @@ class ConversationFragment:BaseFragment() {
     override fun getLayoutResId(): Int = R.layout.fragment_conversation
 
     val conversations = mutableListOf<EMConversation>()
+
+    val messageLister = object : EMMessageListenerAdapter() {
+        override fun onMessageReceived(p0: MutableList<EMMessage>?) {
+            loadConversations()
+        }
+    }
+
         override fun init() {
         super.init()
         headerTitle.text = getString(R.string.message)
@@ -23,16 +32,23 @@ class ConversationFragment:BaseFragment() {
             layoutManager = LinearLayoutManager(context)
             adapter = ConversationListAdapter(context,conversations)
         }
-
+        EMClient.getInstance().chatManager().addMessageListener(messageLister)
         loadConversations()
     }
 
     private fun loadConversations() {
 
         doAsync {
+            // 清空会话列表
+            conversations.clear()
             val allConversations = EMClient.getInstance().chatManager().allConversations
             conversations.addAll(allConversations.values)
             uiThread { recyclerView.adapter?.notifyDataSetChanged() }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        EMClient.getInstance().chatManager().removeMessageListener(messageLister)
     }
 }
