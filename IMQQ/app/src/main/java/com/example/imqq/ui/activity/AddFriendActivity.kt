83d66971af971768f3cf2d7abd4c8a42d7566059
z1572903465 <1572903465@qq.com>
2020-12-27 09:16:41 +0800

    添加好友界面View层的实现

--- IMQQ/app/src/main/java/com/example/imqq/ui/activity/AddFriendActivity.kt
+++ IMQQ/app/src/main/java/com/example/imqq/ui/activity/AddFriendActivity.kt
@@ -3,10 +3,12 @@ package com.example.imqq.ui.activity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.imqq.R
 import com.example.imqq.adapter.AddFriendListAdapter
+import com.example.imqq.contract.AddFriendContract
 import kotlinx.android.synthetic.main.activity_add_friend.*
 import kotlinx.android.synthetic.main.header.*
+import org.jetbrains.anko.toast
 
-class AddFriendActivity :BaseActivity(){
+class AddFriendActivity :BaseActivity(),AddFriendContract.View{
     override fun getLayoutResId(): Int = R.layout.activity_add_friend
 
     override fun init() {
@@ -17,5 +19,17 @@ class AddFriendActivity :BaseActivity(){
             layoutManager = LinearLayoutManager(context)
             adapter = AddFriendListAdapter(context)
         }
+    }
+
+
+    override fun onSearchSuccess() {
+        dismissProgress()
+        toast(R.string.search_failed)
+    }
+
+    override fun onSearchFailed() {
+        dismissProgress()
+        toast(R.string.search_success)
+        recyclerView.adapter?.notifyDataSetChanged()
     }
 }
