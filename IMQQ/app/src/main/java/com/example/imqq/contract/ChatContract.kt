520a5f277d6d17f5384ee54429a8aefd38e8536b
z1572903465 <1572903465@qq.com>
2020-12-27 14:56:44 +0800

    发送一条消息View的实现

---
+++ IMQQ/app/src/main/java/com/example/imqq/contract/ChatContract.kt
@@ -0,0 +1,21 @@
+package com.example.imqq.contract
+
+import com.hyphenate.chat.EMMessage
+
+interface ChatContract{
+
+    interface Presenter: BasePresenter{
+        fun sendMessage(contact: String, message: String)
+        fun addMessage(username: String, p0: MutableList<EMMessage>?)
+        fun loadMessages(username: String)
+        fun loadMoreMessages(username: String)
+    }
+
+    interface View {
+        fun onStartSendMessage()
+        fun onSendMessageSuccess()
+        fun onSendMessageFailed()
+        fun onMessageLoaded()
+        fun onMoreMessageLoaded(size: Int)
+    }
+}
--- IMQQ/app/src/main/java/com/example/imqq/ui/activity/ChatActivity.kt
+++ IMQQ/app/src/main/java/com/example/imqq/ui/activity/ChatActivity.kt
@@ -4,10 +4,12 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import com.example.imqq.R
+import com.example.imqq.contract.ChatContract
 import kotlinx.android.synthetic.main.activity_chat.*
 import kotlinx.android.synthetic.main.header.*
+import org.jetbrains.anko.toast
 
-class ChatActivity:BaseActivity() {
+class ChatActivity:BaseActivity(),ChatContract.View {
     override fun getLayoutResId(): Int = R.layout.activity_chat
 
     override fun init() {
@@ -40,6 +42,33 @@ class ChatActivity:BaseActivity() {
         val username = intent.getStringExtra("username")
         val titleString = String.format(getString(R.string.chat_title),username)
         headerTitle.text = titleString
+
+    }
+
+    override fun onStartSendMessage() {
+        // 通知RecycView刷新列表
+        recyclerView.adapter?.notifyDataSetChanged()
+    }
+
+    override fun onSendMessageSuccess() {
+        // 通知RecycView刷新列表
+        recyclerView.adapter?.notifyDataSetChanged()
+        toast(R.string.send_message_success)
+        // 清空编辑框
+        edit.text.clear()
+    }
+
+    override fun onSendMessageFailed() {
+        toast(R.string.send_message_failed)
+        // 通知RecycView刷新列表
+        recyclerView.adapter?.notifyDataSetChanged()
+    }
+
+    override fun onMessageLoaded() {
+
+    }
+
+    override fun onMoreMessageLoaded(size: Int) {
 
     }
 }
