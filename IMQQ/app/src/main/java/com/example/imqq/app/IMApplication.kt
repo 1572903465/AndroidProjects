3190ea5b5963c0097ac2848d752bd5552bf262cc
z1572903465 <1572903465@qq.com>
2020-12-27 21:10:06 +0800

    完成版本1.0

--- IMQQ/app/src/main/java/com/example/imqq/app/IMApplication.kt
+++ IMQQ/app/src/main/java/com/example/imqq/app/IMApplication.kt
@@ -63,10 +63,10 @@ class IMApplication: Application() {
             }
             val intent = Intent(this, ChatActivity::class.java)
             intent.putExtra("username", it.conversationId())
-            val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+//            val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
-//            val taskStackBuilder = TaskStackBuilder.create(this).addParentStack(ChatActivity::class.java).addNextIntent(intent)
-//            val pendingIntent = taskStackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+            val taskStackBuilder = TaskStackBuilder.create(this).addParentStack(ChatActivity::class.java).addNextIntent(intent)
+            val pendingIntent = taskStackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
             val notification = Notification.Builder(this)
                 .setContentTitle(getString(R.string.receive_new_message))
                 .setContentText(contentText)
@@ -74,7 +74,7 @@ class IMApplication: Application() {
                 .setSmallIcon(R.mipmap.ic_contact)
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true)
-                .notification
+                .build()
             notificationManager.notify(1, notification)
         }
     }
