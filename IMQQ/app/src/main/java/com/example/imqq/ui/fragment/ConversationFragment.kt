7afba7dc8c934d6ed81487b34d99e53954260dff
z1572903465 <1572903465@qq.com>
2020-12-27 17:09:18 +0800

    会话界面的布局

--- IMQQ/app/src/main/java/com/example/imqq/ui/fragment/ConversationFragment.kt
+++ IMQQ/app/src/main/java/com/example/imqq/ui/fragment/ConversationFragment.kt
@@ -1,7 +1,13 @@
 package com.example.imqq.ui.fragment
 
 import com.example.imqq.R
+import kotlinx.android.synthetic.main.header.*
 
 class ConversationFragment:BaseFragment() {
     override fun getLayoutResId(): Int = R.layout.fragment_conversation
+
+    override fun init() {
+        super.init()
+        headerTitle.text = getString(R.string.message)
+    }
 }
