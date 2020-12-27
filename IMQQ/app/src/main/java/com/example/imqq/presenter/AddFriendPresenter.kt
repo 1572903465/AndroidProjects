091f3f2dd0978e3191f9b656e301efb65e1a6f68
z1572903465 <1572903465@qq.com>
2020-12-27 09:29:13 +0800

    添加好友界面Presenter层的实现

---
+++ IMQQ/app/src/main/java/com/example/imqq/presenter/AddFriendPresenter.kt
@@ -0,0 +1,10 @@
+package com.example.imqq.presenter
+
+import com.example.imqq.contract.AddFriendContract
+
+class AddFriendPresenter(val view: AddFriendContract.View): AddFriendContract.Presenter{
+    override fun search(key: String) {
+
+    }
+
+}
--- IMQQ/app/src/main/java/com/example/imqq/ui/activity/AddFriendActivity.kt
+++ IMQQ/app/src/main/java/com/example/imqq/ui/activity/AddFriendActivity.kt
@@ -1,15 +1,19 @@
 package com.example.imqq.ui.activity
 
+import android.view.KeyEvent
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.imqq.R
 import com.example.imqq.adapter.AddFriendListAdapter
 import com.example.imqq.contract.AddFriendContract
+import com.example.imqq.presenter.AddFriendPresenter
 import kotlinx.android.synthetic.main.activity_add_friend.*
 import kotlinx.android.synthetic.main.header.*
 import org.jetbrains.anko.toast
 
 class AddFriendActivity :BaseActivity(),AddFriendContract.View{
     override fun getLayoutResId(): Int = R.layout.activity_add_friend
+    val presenter = AddFriendPresenter(this)
 
     override fun init() {
         super.init()
@@ -19,8 +23,18 @@ class AddFriendActivity :BaseActivity(),AddFriendContract.View{
             layoutManager = LinearLayoutManager(context)
             adapter = AddFriendListAdapter(context)
         }
+        search.setOnClickListener { serach() }
+        userName.setOnEditorActionListener { p0, p1, p2 ->
+            serach()
+            true
+        }
     }
-
+    fun serach(){
+        hideSoftKeyboard()
+        showProgress(getString(R.string.searching))
+        val key = userName.text.trim().toString()
+        presenter.search(key)
+    }
 
     override fun onSearchSuccess() {
         dismissProgress()
