9e4bf9268802bb7a05c33e6b02a9541bf0651f1c
z1572903465 <1572903465@qq.com>
2020-12-27 20:54:01 +0800

    点击通知进入聊天界面

--- IMQQ/app/src/main/java/com/example/imqq/app/IMApplication.kt
+++ IMQQ/app/src/main/java/com/example/imqq/app/IMApplication.kt
@@ -1,18 +1,20 @@
 package com.example.imqq.app
 
-import android.app.ActivityManager
-import android.app.Application
+import android.app.*
 import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
 import android.media.AudioManager
 import android.media.SoundPool
 import cn.bmob.v3.Bmob
-import cn.bmob.v3.util.BmobNotificationManager.showNotification
 import com.example.imqq.BuildConfig
 import com.example.imqq.R
 import com.example.imqq.adapter.EMMessageListenerAdapter
+import com.example.imqq.ui.activity.ChatActivity
 import com.hyphenate.chat.EMClient
 import com.hyphenate.chat.EMMessage
 import com.hyphenate.chat.EMOptions
+import com.hyphenate.chat.EMTextMessageBody
 
 class IMApplication: Application() {
     companion object{
@@ -36,6 +38,7 @@ class IMApplication: Application() {
             } else {
                 //如果在后台，则播放长的声音
                 soundPool.play(yulu, 1f, 1f, 0, 0, 1f)
+                showNotification(p0)
             }
         }
     }
@@ -50,6 +53,32 @@ class IMApplication: Application() {
 
         EMClient.getInstance().chatManager().addMessageListener(messageListener)
     }
+
+    private fun showNotification(p0: MutableList<EMMessage>?) {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        p0?.forEach {
+            var contentText = getString(R.string.no_text_message)
+            if (it.type == EMMessage.Type.TXT){
+                contentText = (it.body as EMTextMessageBody).message
+            }
+            val intent = Intent(this, ChatActivity::class.java)
+            intent.putExtra("username", it.conversationId())
+            val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+//            val taskStackBuilder = TaskStackBuilder.create(this).addParentStack(ChatActivity::class.java).addNextIntent(intent)
+//            val pendingIntent = taskStackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+            val notification = Notification.Builder(this)
+                .setContentTitle(getString(R.string.receive_new_message))
+                .setContentText(contentText)
+                .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.avatar1))
+                .setSmallIcon(R.mipmap.ic_contact)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .notification
+            notificationManager.notify(1, notification)
+        }
+    }
+
 
     private fun isForeground(): Boolean{
         val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
