21dcca9d92fb99ae538f836d87025169db2e4c77
z1572903465 <1572903465@qq.com>
2020-12-27 10:45:26 +0800

    长按弹出删除好友的对话框

--- IMQQ/app/src/main/java/com/example/imqq/adapter/ContactListAdapter.kt
+++ IMQQ/app/src/main/java/com/example/imqq/adapter/ContactListAdapter.kt
@@ -1,10 +1,13 @@
 package com.example.imqq.adapter
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.imqq.R
 import com.example.imqq.data.ContactListItem
 import com.example.imqq.ui.activity.ChatActivity
 import com.example.imqq.widget.ContactListItemView
@@ -28,6 +31,21 @@ class ContactListAdapter(
             intent.putExtra("username",userName)
             context.startActivity(intent)
         }
+        contactListItemView.setOnLongClickListener {
+            val message = String.format(context.getString(R.string.delete_friend_message),userName)
+            AlertDialog.Builder(context)
+                .setTitle(R.string.delete_friend_title)
+                .setMessage(message)
+                .setNegativeButton(R.string.cancel,null)
+                .setPositiveButton(R.string.confirm, DialogInterface.OnClickListener { dialogInterface, i ->
+                    deleteFriend(userName)
+                })
+                .show()
+                 true
+        }
+    }
+    private fun  deleteFriend(userName: String){
+
     }
     class ContactListItemViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
 
